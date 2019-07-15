2089afbbe2bfa5c68d3335647e9fd9231877b8f0
ozcanozkaya <ozcan@solidzone.io>
2019-07-15 21:09:26 +0000

    fixed the typo on wp_public1_subnet wp_public2_subnet

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_subnet" "wp_public1_subnet" {
   vpc_id = "${aws_vpc.wp_vpc.id}"
   cidr_block = "${var.cidrs["public1"]}"
   map_public_ip_on_launch = true
-  availability_zone = "${data.availability_zones.available.names[0]}"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
 
   tags {
     Name = "wp_public1"
@@ -108,7 +108,7 @@ resource "aws_subnet" "wp_public2_subnet" {
   vpc_id = "${aws_vpc.wp_vpc.id}"
   cidr_block = "${var.cidrs["public2"]}"
   map_public_ip_on_launch = true
-  availability_zone = "${data.availability_zones.available.names[1]}"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
 
   tags {
     Name = "wp_public2"
