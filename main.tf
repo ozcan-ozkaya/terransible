e2e8d52fc7f63422d47a13f1c9fb15af2cb70a34
ozcanozkaya <ozcan@solidzone.io>
2019-07-20 21:11:31 +0000

    Added code creating ELB

--- main.tf
+++ main.tf
@@ -404,3 +404,39 @@ EOD
     command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.wp_dev.id} --profile ozcan-root && ansible-playbook -i aws_hosts wordpress.yml"
   }
 }
+
+##### Load Balancer #####
+
+resource "aws_elb" "wp_elb" {
+  name = "${var.domain_name}-elb"
+
+  subnets = ["{aws_subnet.wp_public1_subnet.id}",
+    "${aws_subnet.wp_public2_subnet.id}"
+  ]
+
+  security_groups = ["${aws_security_group.wp_public_sg.id}"]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = "${var.elb_healthy_threshold}"
+    unhealhty_threshold = "${var.elb_unhealthy_threshold}"
+    timeout = "${var.elb_timeout}"
+    target = "TCP:80"
+    interval = "${var.elb_interval}"
+  }
+
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining = true
+
+  tags {
+    Name = "wp_${var.domain_name}-elb"
+  }
+}
